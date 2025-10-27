74147b6cd6804fff1f3788619cf673730b724049
HARSHPARMAR1281 <parmarharsh1281@gmail.com>
2025-10-27 21:14:02 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:    
+    int f=1,s=2;
     int climbStairs(int n) {
-        if(n==1) return n;
-        int arr[n+1];
-        arr[1]=1;
-        arr[2] = 2;
-        for(int i =3;i<=n;i++){
-            arr[i]= arr[i-1]+arr[i-2];
-        }
-       return arr[n];
+    //     if(n==1) return n;
+    //     int arr[n+1];
+    //     arr[1]=1;
+    //     arr[2] = 2;
+    //     for(int i =3;i<=n;i++){
+    //         arr[i]= arr[i-1]+arr[i-2];
+    //     }
+    //    return arr[n];
+    if(n==1||n==2) return n;
+    int sum = 0;
+    for(int i =2;i<n;i++){
+        sum = f+s;
+        f=s;
+        s=sum;
+    }
+    return sum;
 
     }
 };
