f5b3e0844339da78ba4d0a59a5ee9c40cce78730
Harsh <parmarharsh1281@gmail.com>
2026-08-24 11:07:18 +0530

    Time: 11 ms (62.88%), Space: 46.6 MB (50.42%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int ultp(int u , vector<int> & parent){
+        if(parent[u] == u) return u;
+        return parent[u] = ultp(parent[u] , parent);
+    }
+
+    void unionbyrank(vector<int> &parent , vector<int> &rank , int n , vector<vector<int>> &connections){
+        for(auto it : connections){
+            int u = it[0];
+            int v = it[1];
+            int pu = ultp(u , parent);
+            int pv = ultp(v , parent);
+            if(rank[pu] < rank[pv]){
+                parent[pu] = pv;
+                rank[pv] += rank[pu];
+            }
+            else if(rank[pv] < rank[pu]){
+                parent[pv] = pu;
+                rank[pu] += rank[pv];
+            }
+            else {
+                parent[pv] = pu;
+                rank[pu] += rank[pu];
+            }
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n-1) return -1;
+        vector<int> parent(n);
+        for(int i = 0; i < n ;i++){
+            parent[i] = i;
+        }
+        vector<int> arr(n,0);
+        unionbyrank(parent , arr , n , connections);
+        int cables = connections.size();
+        int total_grps = 0;
+        int cables_used = 0;
+        for(int i =0 ;i< n; i++){
+            if(parent[i] == i){
+                total_grps++;
+                cables_used = cables_used + arr[i];
+            }
+        }
+        cables = cables - cables_used;
+        if(cables >= total_grps - 1) return total_grps - 1;
+        else return -1;
+
+
+
+
+        
+
+    }
+};
