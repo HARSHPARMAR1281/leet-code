3782e1cfe6a8d029df4222a66a4b74d36f60c9a2
Harsh <parmarharsh1281@gmail.com>
2026-09-07 10:17:32 +0530

    Time: 8 ms (34.83%), Space: 24.7 MB (32.75%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -16,7 +16,27 @@ public:
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2,-1));
-        return profit(0, 1, prices, n, dp);
+        vector<vector<long>> dp(n+1, vector<long>(2,0));
+        //return profit(0, 1, prices, n, dp); memoisation
+        // here on its tablulation by harsh parmar thakur singh
+
+        
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        //above was base case of reccursion 
+
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy <= 1; buy++){
+                long fayda =0;
+                if(buy){
+                    fayda = max(-prices[idx] + dp[idx+1][0], 0 + dp[idx+1][1]);
+                }
+                else{
+                    fayda = max(prices[idx] + dp[idx+1][1] , 0 + dp[idx+1][0]);
+                }
+                dp[idx][buy] = fayda;
+            }
+        }
+        return dp[0][1];
     }
 };
